bcabe4ddef207177acb31ad049ea3b61320c2f66
eyobsis <ehtube2020@gmail.com>
2024-04-02 01:44:25 +0000

    commit -alx back-end pagibation

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+helper function
+"""
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """
+    Return a tuple of start index and end index for pagination.
+
+    Args:
+        page (int): The current page number (1-indexed).
+        page_size (int): The number of items per page.
+
+    Returns:
+        tuple[int, int]: A tuple containing the start index and end index.
+    """
+    start_index: int = (page - 1) * page_size
+    end_index: int = start_index + page_size
+    return start_index, end_index
