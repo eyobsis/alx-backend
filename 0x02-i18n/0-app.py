810fbc818a4b5bba8e5f86885bb39feed41e4b9d
eyobsis <ehtube2020@gmail.com>
2024-04-12 17:18:24 -0700

    edted

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 """
-A simple Flask application serving a basic HTML template.
+A Basic flask application
 """
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
-    Renders the index HTML template.
+    Renders a basic html template
     """
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
